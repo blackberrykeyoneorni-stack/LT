2212bb9abc3dff54f27705604c62b6b9d3042882
blackberrykeyoneorni-stack <blackberry.keyone.orni@gmail.com>
2026-02-06 10:27:33 +0100

    Schuldenfalle v5

---
+++ dist/assets/listItemTextClasses-BD3o8rje.js
@@ -0,0 +1,1 @@
+import{g as e,b as s}from"./index-T6og8xNo.js";function a(t){return s("MuiListItemText",t)}const l=e("MuiListItemText",["root","multiline","dense","inset","primary","secondary"]);export{a as g,l};
